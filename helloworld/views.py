f283ec1074ca4cbd897a867775e0607e1af6e636
maryamhed <maryamh95@gmail.com>
2018-09-28 11:36:06 -0400

    Embed placeholder imgur post in html

--- helloworld/views.py
+++ helloworld/views.py
@@ -2,8 +2,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context_instance=None)
+
+class ImgurPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'imgur.html')
